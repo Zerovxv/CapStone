bff6350b3f9901e9fd008c6e52eba7e7dc33e61a
Caleb Butcher <cbutch17@aol.com>
2022-11-03 09:56:39 -0400

    2nd commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './misc/home/home.component';
+import { AboutComponent } from './misc/about/about.component';
+import { E404Component } from './misc/e404/e404.component';
+import { MenuComponent } from './misc/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    E404Component,
+    MenuComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,FormsModule,HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
